b01e12f19a748470fa565d9ee207937ef59a7c17
Toma Rosa <toma.rosa@ik.me>
2024-12-18 16:56:38 +0100

    chore: split and chunks build

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,19 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
